9c3285acded4971567fd4ee52b035ca59025d2ca
Tfg Developer <tfgebookdeveloper@gmail.com>
2026-08-31 22:39:46 +0500

    Sitemap ura dywa

--- src/Components/Ctasection.jsx
+++ src/Components/Ctasection.jsx
@@ -7,16 +7,14 @@ function Ctasection() {
 
   return (
     <motion.div
-      className="w-full py-10 px-4 md:px-16 flex flex-col lg:flex-row md:flex-row sm:flex-col items-center justify-between bg-cover bg-center gap-5"
-      style={{
-        backgroundImage: "url('/images/EXCLUSIVE-oFFER.jpg')",
-        height: "200px"
-      }}
+      className="w-full flex flex-col lg:flex-row md:flex-row sm:flex-col items-center justify-between bg-contain bg-center gap-5"
+      
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true, amount: 0.3 }} // triggers when 30% of the section is in view
       transition={{ duration: 0.8, ease: "easeOut" }}
     >
+      <img src="/images/EXCLUSIVE-oFFER.jpg" className="w-full h-50 md:h-auto"/>
     </motion.div>
   );
 }
